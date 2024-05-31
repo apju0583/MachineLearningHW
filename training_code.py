6142764ea42472a77a58ceefac37ecb1b6878ba9
apju0583 <62836627+apju0583@users.noreply.github.com>
2024-05-31 22:52:27 +0900

    Update training_code.py

--- training_code.py
+++ training_code.py
@@ -2,17 +2,22 @@
 
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="--------------------") // Roboflow API 키 (숨김처리)
+# Roboflow API 키 (숨김처리)
+rf = Roboflow(api_key="--------------------") 
 
 project = rf.workspace("ml-hw-labelling").project("ml-hw-label")
 version = project.version(1)
 
-dataset = version.download("yolov8") // YOLOv8 형식의 데이터셋 다운로드
+# YOLOv8 형식의 데이터셋 다운로드
+dataset = version.download("yolov8") 
 
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.yaml') // YOLOv8 모델 불러오기
+# YOLOv8 모델 불러오기
+model = YOLO('yolov8n.yaml') 
 
-data_path = dataset.location + '/data.yaml' // 데이터 경로 설정
+# 데이터 경로 설정
+data_path = dataset.location + '/data.yaml' 
 
-model.train(data=data_path, epochs=100, imgsz=640) // 모델 훈련
+# 모델 훈련
+model.train(data=data_path, epochs=100, imgsz=640)
